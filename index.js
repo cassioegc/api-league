0667d4e39a701db420c11aaa8d2d4e7ae4b29c87
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2021-10-27 09:06:13 -0300

    add routes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routes = require('./routes');
 
 const PORT = process.env.port || 3000;
 
@@ -6,8 +7,9 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use('/', routes);
 
-app.listen(config.port, () => {
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
