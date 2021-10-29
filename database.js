222ee07bfcc2a05a52c4ae422200200b3bfe655b
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2021-10-29 12:49:19 -0300

    add db connection and skin model

---
+++ database.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.Promise = Promise
+
+const isDisconnected = 0
+
+mongoose.connection.on('connected', () => {
+  console.log('Connection Established')
+})
+
+mongoose.connection.on('reconnected', () => {
+  console.log('Connection Reestablished')
+})
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Connection Disconnected')
+})
+
+mongoose.connection.on('close', () => {
+  console.log('Connection Closed')
+})
+mongoose.connection.on('error', (error) => {
+  console.log('ERROR: ' + error)
+})
+
+console.log('-------------------------------------')
+console.log(`${process.env.DATABASE_URL}`)
+console.log('-------------------------------------')
+
+if (mongoose.connection.readyState === isDisconnected) {
+  mongoose.connect(`${process.env.DATABASE_URL}`, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+}
+
+module.exports = mongoose
